aa238040e73a527e7049fff5e6a645154764f103
Rohan Lokhande <rohanlokhande1306@gmail.com>
2025-11-29 09:41:15 +0530

    Added VariablesExample.java to practice basic Java variables and datatypes
    Created a new file to practice different variable types in Java, including int, double, char, boolean, and String.

--- 02-variables/VariablesExample.java
+++ 02-variables/VariablesExample.java
@@ -1,1 +1,20 @@
+public class VariablesExample 
+{
+  public static void main(String[] args) 
+     {
 
+        int age = 20;
+        double price = 99.99;
+        char grade = 'A';
+        String name = "Rohan";
+        boolean isStudent = true;
+       
+        // Printing All variables 
+       
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+     }
+}
